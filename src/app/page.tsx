861e3c3ef5784efe411d2abb50b13004c9637720
Aridan <aridanpantoja@gmail.com>
2024-09-07 17:43:07 -0300

    feat: add hero-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { HeroSection } from '@/components/hero-section'
+
 export default function Home() {
-  return <h1>Home</h1>
+  return <HeroSection />
 }
---
+++ src/components/hero-section/index.tsx
@@ -0,0 +1,33 @@
+import { Section } from '@/components/section'
+import Link from 'next/link'
+import { buttonVariants } from '../shadcn-ui/button'
+
+export function HeroSection() {
+  return (
+    <Section className="md:my-40">
+      <div className="max-w-3xl space-y-6 md:space-y-8">
+        <h1 className="text-center">
+          Conectando ideias,
+          <br />
+          Impulsionando soluções
+        </h1>
+        <p className="text-lg text-muted-foreground md:text-2xl">
+          Entre em contato conosco para discutir como podemos ajudar a
+          transformar sua presença online!
+        </p>
+        <div className="flex flex-wrap items-center justify-center gap-6">
+          <Link href="/contato" className={buttonVariants()}>
+            Solicite seu orçamento
+          </Link>
+
+          <Link
+            href="#servicos"
+            className={buttonVariants({ variant: 'secondary' })}
+          >
+            Ver serviços &rarr;
+          </Link>
+        </div>
+      </div>
+    </Section>
+  )
+}
