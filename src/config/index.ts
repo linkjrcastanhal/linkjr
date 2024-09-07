cf56ffdcc96a43ab73e5c0f7b8e6e1734b5553c1
Aridan <aridanpantoja@gmail.com>
2024-09-07 20:06:09 -0300

    feat: add team section

--- src/config/index.ts
+++ src/config/index.ts
@@ -16,3 +16,51 @@ export const PAGES = [
     href: '/contato',
   },
 ]
+
+export const TEAM = [
+  {
+    name: 'Alícia Maia',
+    role: 'Diretora-presidente',
+    image: '/images/team/alicia.webp',
+  },
+  {
+    name: 'Karol Wojtyla',
+    role: 'Vice-presidente',
+    image: '/images/team/karol.webp',
+  },
+  {
+    name: 'Ítalo Melo',
+    role: 'Diretor Financeiro',
+    image: '/images/team/italo.webp',
+  },
+  {
+    name: 'Linda Nowaczyk',
+    role: 'Diretora de Marketing',
+    image: '/images/team/linda.webp',
+  },
+  {
+    name: 'Beatriz Abreu',
+    role: 'Diretora de Pessoas',
+    image: '/images/team/beatriz.webp',
+  },
+  {
+    name: 'Sarah Cabral',
+    role: 'Diretora de UX/UI',
+    image: '/images/team/sarah.webp',
+  },
+  {
+    name: 'Aridan Pantoja',
+    role: 'Diretor de Projetos',
+    image: '/images/team/aridan.webp',
+  },
+  {
+    name: 'Edson Costa',
+    role: 'Equipe de Projetos',
+    image: '/images/team/edson.webp',
+  },
+  {
+    name: 'Nycolas Bastos',
+    role: 'Equipe de Projetos',
+    image: '/images/team/nycolas.webp',
+  },
+]
