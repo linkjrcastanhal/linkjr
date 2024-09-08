49b1274adaa89f8fb2b39740da56533d70adf987
Aridan <aridanpantoja@gmail.com>
2024-09-08 00:05:19 -0300

    style: change card padding

--- src/components/home/team/index.tsx
+++ src/components/home/team/index.tsx
@@ -8,7 +8,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '@/components/shadcn-ui/carousel'
-import { Card, CardContent } from '@/components/shadcn-ui/card'
+import { Card } from '@/components/shadcn-ui/card'
 import Image from 'next/image'
 import { TEAM } from '@/config'
 import { useRef } from 'react'
@@ -33,18 +33,16 @@ export function Team() {
               className="basis-full min-[480px]:basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
               key={item.name}
             >
-              <Card>
-                <CardContent className="flex flex-col items-center justify-center rounded-xl p-6">
-                  <Image
-                    src={item.image}
-                    alt={item.name}
-                    width={256}
-                    height={256}
-                    className="size-32 rounded-full border"
-                  />
-                  <p className="mt-4 text-xl font-bold">{item.name}</p>
-                  <p className="mt-1 text-base">{item.role}</p>
-                </CardContent>
+              <Card className="flex flex-col items-center justify-center rounded-xl p-6">
+                <Image
+                  src={item.image}
+                  alt={item.name}
+                  width={256}
+                  height={256}
+                  className="size-32 rounded-full border"
+                />
+                <p className="mt-4 text-xl font-bold">{item.name}</p>
+                <p className="mt-1 text-base">{item.role}</p>
               </Card>
             </CarouselItem>
           ))}
